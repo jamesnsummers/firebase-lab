5850c46053b85997f2c0674967421b01119056b9
James Summers <jamesnsummers@gmail.com>
2017-06-15 15:18:41 -0500

    added being nice to the greeting

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ class Header extends Component {
   render (){
     return (
       <div>
-        <h1>Welcome to My Firebase App</h1>
-        {this.props.currentUser !=null ? <h1>{this.props.currentUser.displayName}</h1> : <h1>You're a stranger.</h1>}
+        <h3>Welcome to My Firebase App</h3>
+        {this.props.currentUser !=null ? <h2>Hello {this.props.currentUser.displayName}</h2> : <h1>You're a stranger.</h1>}
         <button onClick={event => this.props.loginButtonClicked(event)}>Login</button>{' '}
         <button onClick={event => this.props.logoutButtonClicked(event)}>Logout</button>
       </div>
